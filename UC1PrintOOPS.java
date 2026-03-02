efd9fd7d8907825c74ce61d08e4a688253b87c87
Palak <pz8937@srmist.edu.in>
2026-03-02 23:23:29 +0530

    new file

---
+++ UC1PrintOOPS.java
@@ -0,0 +1,13 @@
+pulbic class OOPSBannerApp {
+    public static void main(String[] args){
+        systems.out.println(x: "OOPS");
+    }
+
+System.out.println("   ***    " + " " + "  *****  ");
+System.out.println(" **   **  " + " " + " **      ");
+System.out.println("**     ** " + " " + "**       ");
+System.out.println("**     ** " + " " + "  *****  ");
+System.out.println("**     ** " + " " + "      ** ");
+System.out.println(" **   **  " + " " + " **   ** ");
+System.out.println("   ***    " + " " + " *****   ");
+}
