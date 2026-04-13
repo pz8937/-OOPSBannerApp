d9cd33b25649a1be83ecbcbe3363c4b2efe37805
Palak <pz8937@srmist.edu.in>
2026-04-13 15:16:20 +0530

    this usecase 5 is soln

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,18 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        String[] lines = new String[];
 
-        lines[0] = String.join(" ", " *** ", " *** ", " **** ", " *** ");
-        lines[1] = String.join(" ", "*   *", "*   *", "*   *", "*    ");
-        lines[2] = String.join(" ", "*   *", "*   *", "*   *", "*    ");
-        lines[3] = String.join(" ", "*   *", "*   *", " **** ", " *** ");
-        lines[4] = String.join(" ", "*   *", "*   *", "*      ", "    *");
-        lines[5] = String.join(" ", "*   *", "*   *", "*      ", "    *");
-        lines[6] = String.join(" ", " *** ", " *** ", "*      ", " *** ");
+        String[] lines = {
+            String.join(" ", " ***  ", " ***** ", " ***** ", " ***** "),
+            String.join(" ", "*   * ", "*   * ", "*   * ", "*     "),
+            String.join(" ", "*   * ", "*   * ", "*   * ", "*     "),
+            String.join(" ", "*   * ", " ***** ", " ***** ", " ***** "),
+            String.join(" ", "*   * ", " *     ", "     * ", "     * "),
+            String.join(" ", "*   * ", " *     ", "     * ", "     * "),
+            String.join(" ", " ***  ", " ***** ", " ***** ", " ***** ")
+        };
 
+        // Print each line using for-each loop
         for (String line : lines) {
             System.out.println(line);
         }
